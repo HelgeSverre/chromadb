43c6862367c1b53fdb7e5640403ee3b7d3749578
Helge Sverre <helge.sverre@gmail.com>
2023-12-29 09:37:32 +0100

    aider: Refactored resource methods to accept additional parameters and improved method calls.
    # Aider chat conversation:
    
    ASSISTANT: ```diff
    --- src/Resource/Items.php
    +++ src/Resource/Items.php
    @@ ... @@
     class Items extends BaseResource
     {
    -    public function add(string $collectionId): Response
    +    public function add(
    +        string $collectionId,
    +        ?array $ids = null,
    +        ?array $embeddings = null,
    +        ?array $metadatas = null,
    +        ?array $documents = null
    +    ): Response
         {
    -        return $this->connector->send(new AddItems($collectionId));
    +        return $this->connector->send(new AddItems(
    +            collectionId: $collectionId,
    +            ids: $ids,
    +            embeddings: $embeddings,
    +            metadatas: $metadatas,
    +            documents: $documents
    +        ));
         }
    
    -    public function update(string $collectionId): Response
    +    public function update(
    +        string $collectionId,
    +        array $ids,
    +        ?array $embeddings = null,
    +        ?array $metadatas = null,
    +        ?array $documents = null
    +    ): Response
         {
    -        return $this->connector->send(new UpdateItems($collectionId));
    +        return $this->connector->send(new UpdateItems(
    +            collectionId: $collectionId,
    +            ids: $ids,
    +            embeddings: $embeddings,
    +            metadatas: $metadatas,
    +            documents: $documents
    +        ));
         }
    
    -    public function upsert(string $collectionId): Response
    +    public function upsert(
    +        string $collectionId,
    +        array $ids,
    +        ?array $embeddings = null,
    +        ?array $metadatas = null,
    +        ?array $documents = null
    +    ): Response
         {
    -        return $this->connector->send(new UpsertItems($collectionId));
    +        return $this->connector->send(new UpsertItems(
    +            collectionId: $collectionId,
    +            ids: $ids,
    +            embeddings: $embeddings,
    +            metadatas: $metadatas,
    +            documents: $documents
    +        ));
         }
    
    -    public function get(string $collectionId): Response
    +    public function get(
    +        string $collectionId
    +    ): Response
         {
    -        return $this->connector->send(new GetItems($collectionId));
    +        return $this->connector->send(new GetItems(
    +            collectionId: $collectionId
    +        ));
         }
    
    -    public function delete(string $collectionId): Response
    +    public function delete(
    +        string $collectionId,
    +        array $ids = [],
    +        array $where = [],
    +        array $whereDocument = []
    +    ): Response
         {
    -        return $this->connector->send(new DeleteItems($collectionId));
    +        return $this->connector->send(new DeleteItems(
    +            collectionId: $collectionId,
    +            ids: $ids,
    +            where: $where,
    +            whereDocument: $whereDocument
    +        ));
         }
    
    -    public function count(string $collectionId): Response
    +    public function count(
    +        string $collectionId
    +    ): Response
         {
    -        return $this->connector->send(new CountItems($collectionId));
    +        return $this->connector->send(new CountItems(
    +            collectionId: $collectionId
    +        ));
         }
    
    -    public function query(string $collectionId): Response
    +    public function query(
    +        string $collectionId,
    +        array $queryEmbeddings,
    +        array $where = [],
    +        array $whereDocument = [],
    +        array $include = ['embeddings', 'documents', 'metadatas'],
    +        int $nResults = 10
    +    ): Response
         {
    -        return $this->connector->send(new QueryItems($collectionId));
    +        return $this->connector->send(new QueryItems(
    +            collectionId: $collectionId,
    +            queryEmbeddings: $queryEmbeddings,
    +            where: $where,
    +            whereDocument: $whereDocument,
    +            include: $include,
    +            nResults: $nResults
    +        ));
         }
     }
    ```

--- src/Resource/Items.php
+++ src/Resource/Items.php
@@ -14,38 +14,106 @@ use Saloon\Http\Response;
 
 class Items extends BaseResource
 {
-    public function add(string $collectionId): Response
+    public function add(
+        string $collectionId,
+        ?array $ids = null,
+        ?array $embeddings = null,
+        ?array $metadatas = null,
+        ?array $documents = null
+    ): Response
     {
-        return $this->connector->send(new AddItems($collectionId));
+        return $this->connector->send(new AddItems(
+            collectionId: $collectionId,
+            ids: $ids,
+            embeddings: $embeddings,
+            metadatas: $metadatas,
+            documents: $documents
+        ));
     }
 
-    public function update(string $collectionId): Response
+    public function update(
+        string $collectionId,
+        array $ids,
+        ?array $embeddings = null,
+        ?array $metadatas = null,
+        ?array $documents = null
+    ): Response
     {
-        return $this->connector->send(new UpdateItems($collectionId));
+        return $this->connector->send(new UpdateItems(
+            collectionId: $collectionId,
+            ids: $ids,
+            embeddings: $embeddings,
+            metadatas: $metadatas,
+            documents: $documents
+        ));
     }
 
-    public function upsert(string $collectionId): Response
+    public function upsert(
+        string $collectionId,
+        array $ids,
+        ?array $embeddings = null,
+        ?array $metadatas = null,
+        ?array $documents = null
+    ): Response
     {
-        return $this->connector->send(new UpsertItems($collectionId));
+        return $this->connector->send(new UpsertItems(
+            collectionId: $collectionId,
+            ids: $ids,
+            embeddings: $embeddings,
+            metadatas: $metadatas,
+            documents: $documents
+        ));
     }
 
-    public function get(string $collectionId): Response
+    public function get(
+        string $collectionId
+    ): Response
     {
-        return $this->connector->send(new GetItems($collectionId));
+        return $this->connector->send(new GetItems(
+            collectionId: $collectionId
+        ));
     }
 
-    public function delete(string $collectionId): Response
+    public function delete(
+        string $collectionId,
+        array $ids = [],
+        array $where = [],
+        array $whereDocument = []
+    ): Response
     {
-        return $this->connector->send(new DeleteItems($collectionId));
+        return $this->connector->send(new DeleteItems(
+            collectionId: $collectionId,
+            ids: $ids,
+            where: $where,
+            whereDocument: $whereDocument
+        ));
     }
 
-    public function count(string $collectionId): Response
+    public function count(
+        string $collectionId
+    ): Response
     {
-        return $this->connector->send(new CountItems($collectionId));
+        return $this->connector->send(new CountItems(
+            collectionId: $collectionId
+        ));
     }
 
-    public function query(string $collectionId): Response
+    public function query(
+        string $collectionId,
+        array $queryEmbeddings,
+        array $where = [],
+        array $whereDocument = [],
+        array $include = ['embeddings', 'documents', 'metadatas'],
+        int $nResults = 10
+    ): Response
     {
-        return $this->connector->send(new QueryItems($collectionId));
+        return $this->connector->send(new QueryItems(
+            collectionId: $collectionId,
+            queryEmbeddings: $queryEmbeddings,
+            where: $where,
+            whereDocument: $whereDocument,
+            include: $include,
+            nResults: $nResults
+        ));
     }
 }
