edd1a9b098bb95a9d98a24ba7f7ed158aa6e4ffb
Helge Sverre <helge.sverre@gmail.com>
2023-12-29 13:40:58 +0100

    wip

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -8,6 +8,9 @@ beforeEach(function () {
         host: 'http://localhost',
         port: '8000'
     );
+
+    // Reset the server before each test
+    $this->chromadb->server()->reset();
 });
 
 it('counts collections correctly', function () {
@@ -15,34 +18,60 @@ it('counts collections correctly', function () {
     $this->chromadb->collections()->create('test_collection_for_count');
     $countAfterCreate = $this->chromadb->collections()->count();
     expect($countAfterCreate)->toEqual($initialCount + 1);
+
     $this->chromadb->collections()->delete('test_collection_for_count');
     $countAfterDelete = $this->chromadb->collections()->count();
     expect($countAfterDelete)->toEqual($initialCount);
 });
 
 it('retrieves a collection correctly', function () {
-    $this->chromadb->collections()->create('test_collection_for_retrieve');
-    $response = $this->chromadb->collections()->get('test_collection_for_retrieve');
-    expect($response->json('name'))->toEqual('test_collection_for_retrieve');
-    $this->chromadb->collections()->delete('test_collection_for_retrieve');
+    $create = $this->chromadb->collections()->create('test_collection_for_retrieve');
+    expect($create->ok())->toBeTrue();
+
+    $get = $this->chromadb->collections()->get('test_collection_for_retrieve');
+    expect($get->ok())->toBeTrue()
+        ->and($get->json('name'))->toEqual('test_collection_for_retrieve');
+
 });
 
 it('lists collections correctly', function () {
-    $this->chromadb->collections()->create('test_collection_for_list');
-    $response = $this->chromadb->collections()->list();
-    expect($response->collect()->pluck('name'))->toContain('test_collection_for_list');
-    $this->chromadb->collections()->delete('test_collection_for_list');
+    $create = $this->chromadb->collections()->create('test_collection_for_list');
+    expect($create->ok())->toBeTrue();
+
+    $list = $this->chromadb->collections()->list();
+    expect($list->collect()->pluck('name'))->toContain('test_collection_for_list');
 });
 
-it('updates a collection correctly', function () {
-    $response = $this->chromadb->collections()->create('test_collection_for_update', getOrCreate: true);
-    $collectionId = $response->json('id');
-    $updateResponse = $this->chromadb->collections()->update(
-        collectionId: $collectionId,
+it('updates a collection metadata correctly', function () {
+    $create = $this->chromadb->collections()->create('test_collection_for_update');
+    expect($create->ok())->toBeTrue();
+
+    $update = $this->chromadb->collections()->update(
+        collectionId: $create->json('id'),
         newMetadata: ['updated' => '100']
     );
-    expect($updateResponse->status())->toEqual(200);
-    $getResponse = $this->chromadb->collections()->get('test_collection_for_update');
-    expect($getResponse->json('metadata.updated'))->toEqual(100);
-    $this->chromadb->collections()->delete('test_collection_for_update');
+    expect($update->ok())->toBeTrue();
+
+    $get = $this->chromadb->collections()->get('test_collection_for_update');
+
+    expect($get->ok())->toBeTrue()
+        ->and($get->json('name'))->toEqual('test_collection_for_update')
+        ->and($get->json('metadata.updated'))->toEqual(100);
+});
+
+it('updates a collection name correctly', function () {
+    $create = $this->chromadb->collections()->create('test_collection_for_update');
+    expect($create->ok())->toBeTrue();
+
+    $update = $this->chromadb->collections()->update(
+        collectionId: $create->json('id'),
+        newName: 'test_collection_for_update_2'
+    );
+    expect($update->ok())->toBeTrue();
+
+    $get = $this->chromadb->collections()->get('test_collection_for_update_2');
+
+    expect($get->ok())->toBeTrue()
+        ->and($get->json('name'))->toEqual('test_collection_for_update_2');
+
 });
